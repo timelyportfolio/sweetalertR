20b3bbae1938db167d95813e90878d140c5e7413
timelyportfolio <kent.russell@timelyportfolio.com>
2015-06-29 14:30:44 -0500

    add minimal roxygen documentation

--- R/sweetalert.R
+++ R/sweetalert.R
@@ -1,6 +1,64 @@
-#' <Add Title>
+#' sweetalert in R as an htmlwidget
 #'
-#' <Add Description>
+#' Create sweetalerts in R with this function.  For details on the arguments, see the
+#' \href{http://t4t5.github.io/sweetalert/}{SweetAlert demo}.
+#'
+#' @examples
+#' \dontrun{
+#' library(sweetalertR)
+#' library(htmltools)
+#'
+#'
+#' # do the first sweetalert example using a tagList
+#' #  and sweetalert with a selector
+#' #  This is the advisable way for most R users
+#' browsable(tagList(
+#'   HTML('
+#'    <div class="showcase sweet">
+#'      <h4>Sweet Alert</h4>
+#'      <button>Show error message</button>
+#'
+#'      <h5>R Code:</h5>
+#'      <pre>
+#'        sweetalert(
+#'         selector = ".showcase.sweet button"
+#'         ,title = "Oops..."
+#'         ,text =  "Something went wrong!"
+#'         ,type = "error"
+#'        )
+#'      </pre>
+#'    </div>
+#'   ')
+#'   ,sweetalert(
+#'     selector = '.showcase.sweet button'
+#'     ,title = "Oops..."
+#'     ,text =  "Something went wrong!"
+#'     ,type = "error"
+#'   )
+#' ))
+#'
+#' # do the first sweetalert example using a tagList
+#' #   but with sweetalert( selector = NULL )
+#' #   assuming someone knows HTML and JavaScript
+#' browsable(tagList(
+#' HTML('
+#' <div class="showcase sweet">
+#' 	<h4>Sweet Alert</h4>
+#' 	<button onclick = "sweetAlert( \'Oops...\',\'Something went wrong!\',\'error\')">
+#'     Show error message
+#'   </button>
+#'
+#' 	<h5>R Code:</h5>
+#' 	<pre>
+#'   # no selector so sweetalert htmlwidget just used for dependencies
+#'   sweetalert()
+#'   </pre>
+#' </div>
+#'   ')
+#'   ,sweetalert()
+#' ))
+#' }
+#'
 #'
 #' @import htmlwidgets
 #'
